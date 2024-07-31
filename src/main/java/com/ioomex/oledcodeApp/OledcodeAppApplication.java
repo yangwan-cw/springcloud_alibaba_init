e70e9018a1a10e64f4a61079cf2e0fc070bbb6ab
ioomex <a2572213252@gmail.com>
2024-07-31 13:57:18 +0800

    init

---
+++ src/main/java/com/ioomex/oledcodeApp/OledcodeAppApplication.java
@@ -0,0 +1,13 @@
+package com.ioomex.oledcodeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OledcodeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OledcodeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ioomex/oledcodeApp/OledcodeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioomex.oledcodeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OledcodeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
